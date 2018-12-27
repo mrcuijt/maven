7188a0ad2c0a219339e451652386c775781d06cf
mrcuijt <mrcuijt@gmail.com>
2018-12-27 16:46:40 +0800

    1、更新 ToUpdateReleaseHouseServlet.java 1 1）更新参数判断逻辑 2）添加日志记录

--- lease-of-house/src/main/java/science/mrcuijt/loh/servlet/loh/lease/ToUpdateReleaseHouseServlet.java
+++ lease-of-house/src/main/java/science/mrcuijt/loh/servlet/loh/lease/ToUpdateReleaseHouseServlet.java
@@ -4,12 +4,17 @@
 package science.mrcuijt.loh.servlet.loh.lease;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import science.mrcuijt.loh.entity.LohFileInfo;
 import science.mrcuijt.loh.entity.LohHouseInfo;
@@ -24,11 +29,15 @@ import science.mrcuijt.loh.service.impl.LohServiceImpl;
  */
 public class ToUpdateReleaseHouseServlet extends HttpServlet {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ToUpdateReleaseHouseServlet.class);
+
 	LohService lohService = new LohServiceImpl();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		boolean debug = LOG.isDebugEnabled();
 
 		// 获取登录用户标识
 		Integer loginInfoId = (Integer) request.getSession().getAttribute("login_info_id");
@@ -38,72 +47,101 @@ public class ToUpdateReleaseHouseServlet extends HttpServlet {
 		// 获取修改房屋的 ID
 		Integer lohHouseId = null;
 
+		// 请求参数验证结果
+		boolean verifyResult = true;
+
 		// 消息
 		String message = null;
 
 		try {
-			lohHouseId = Integer.parseInt(request.getParameter("id"));
+			String paramId = request.getParameter("id");
+			if (paramId != null && paramId.trim().length() > 0) {
+				LOG.info("if(paramId != null && paramId.trim().length() > 0) [{}]", (paramId != null && paramId.trim().length() > 0));
+				paramId = paramId.trim();
+				lohHouseId = Integer.parseInt(paramId);
+			}
 		} catch (NumberFormatException e) {
-			e.printStackTrace();
+			LOG.info("Convert lohHouseId [{}] has error", request.getParameter("id"));
+			verifyResult = false;
+			message = "Parameter Not Support";
 		}
 
-		// 验证参数
-		if (lohHouseId == null) {
+		// 验证请求参数
+		if (verifyResult) {
+			if (lohHouseId == null || lohHouseId <= 0) {
+				LOG.info("if (lohHouseId == null) [{}]", (lohHouseId == null));
+				message = "Parameter Not Support";
+				verifyResult = false;
+			}
+		}
 
-			message = "房屋信息不存在";
-			response.sendRedirect(request.getContextPath() + "/loh/lease/main.do");
+		// 验证房屋信息是否存在
+		LohHouseInfo lohHouseInfo = null;
+		if (verifyResult) {
+			LOG.info("查询指定房屋租赁信息[{}]", lohHouseId);
+			lohHouseInfo = lohService.findLohHouseInfoByPrimaryKey(lohHouseId);
+			if (lohHouseInfo == null) {
+				LOG.info("if (lohHouseInfo == null) [{}]", (lohHouseInfo == null));
+				verifyResult = false;
+				message = "房屋租赁信息记录不存在，请重试。";
+			}
+		}
+
+		// 验证房屋信息是否属于当前登录用户
+		if (verifyResult) {
+			if (userInfoId.intValue() != lohHouseInfo.getUserInfoId().intValue()) {
+				LOG.info("if (userInfoId.intValue() != lohHouseInfo.getUserInfoId().intValue()) [{}]", (userInfoId.intValue() != lohHouseInfo.getUserInfoId().intValue()));
+				verifyResult = false;
+				message = "当前用户没有权限修改该房屋信息。";
+			}
+		}
+
+		// 验证不通过
+		if (!verifyResult) {
+			LOG.info("if (!verifyResult) [{}]", (!verifyResult));
+			message = URLEncoder.encode(message, "UTF-8");
+			response.sendRedirect(request.getContextPath() + "/loh/lease/main.do?message=" + message);
+			LOG.info("response.sendRedirect({}\"/loh/lease/main.do?message={}\")", request.getContextPath(), URLDecoder.decode(message,"UTF-8"));
 			return;
 		}
 
-		// 查询房屋信息是否存在
-		LohHouseInfo lohHouseInfo = lohService.findLohHouseInfoByPrimaryKey(lohHouseId);
-
-		if (lohHouseInfo == null) {
-
-			message = "房屋信息不存在";
-			response.sendRedirect(request.getContextPath() + "/loh/lease/main.do");
-			return;
-		}
-
-		// 查询房屋信息是否属于当前登录用户
-		if (userInfoId.intValue() != lohHouseInfo.getUserInfoId().intValue()) {
-
-			message = "当前用户没有权限修改该房屋信息。";
-			response.sendRedirect(request.getContextPath() + "/loh/lease/main.do");
-			return;
-		}
-
+		LOG.info("查询指定房屋类型[{}]", lohHouseInfo.getLohHouseTypeId());
 		// 查询房屋类型
 		LohHouseType lohHouseType = lohService.findLohHouseTypeByPrimaryKey(lohHouseInfo.getLohHouseTypeId());
-		
+
+		LOG.info("查询全部房屋类型");
 		// 查询房屋类型列表
 		List<LohHouseType> LohHouseTypeList = lohService.findAllLohHouseType();
-		
+
 		// 加载地区信息
 		List<RegionInfo> provinceList = null;
 		List<RegionInfo> cityList = null;
 		List<RegionInfo> countyList = null;
-		
+
+		LOG.info("查询地区级别为[{}]的地区信息", 1);
 		provinceList = lohService.findRegionInfoByLevel(1);
-		
-		if(lohHouseInfo.getRegionInfoCityId() != null && lohHouseInfo.getRegionInfoCityId() > 0) {
+
+		if (lohHouseInfo.getRegionInfoCityId() != null && lohHouseInfo.getRegionInfoCityId() > 0) {
+			LOG.info("if (lohHouseInfo.getRegionInfoCityId() != null && lohHouseInfo.getRegionInfoCityId() > 0) [{}]",  (lohHouseInfo.getRegionInfoCityId() != null && lohHouseInfo.getRegionInfoCityId() > 0));
+			LOG.info("查询指定地区信息[{}]", lohHouseInfo.getRegionInfoCityId());
 			RegionInfo regionInfo = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCityId());
+			LOG.info("查询相同父级地区[{}]的地区信息列表", regionInfo.getParentRegionId());
 			cityList = lohService.findRegionInfoByParentRegionId(regionInfo.getParentRegionId());
 		}
-		
+
 		if(lohHouseInfo.getRegionInfoCountyId() != null && lohHouseInfo.getRegionInfoCountyId() > 0) {
+			LOG.info("查询指定地区信息[{}]", lohHouseInfo.getRegionInfoCountyId());
 			RegionInfo regionInfo = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCountyId());
+			LOG.info("查询相同父级地区[{}]的地区信息列表", regionInfo.getParentRegionId());
 			countyList = lohService.findRegionInfoByParentRegionId(regionInfo.getParentRegionId());
 		}
-		
-		
+
 		// 查询房屋信息关联的房屋文件信息
+		LOG.info("查询房屋信息[{}]关联的房屋文件信息", lohHouseId);
 		List<LohFileInfo> lohFileInfos = lohService.findLohFileInfoByLohHouseInfoId(lohHouseId);
 
 		// 添加房屋信息
 		request.setAttribute("lohHouseInfo", lohHouseInfo);
-		// 添加房屋类型
-		
 		// 添加所有的房屋类型
 		request.setAttribute("LohHouseTypeList", LohHouseTypeList);
 		// 添加地区信息
@@ -114,6 +152,7 @@ public class ToUpdateReleaseHouseServlet extends HttpServlet {
 		request.setAttribute("lohFileInfos", lohFileInfos);
 
 		request.getRequestDispatcher("/WEB-INF/html/loh/lease/to_update_release_house.jsp").forward(request, response);
+		LOG.info("request.getRequestDispatcher(\"/WEB-INF/html/loh/lease/to_update_release_house.jsp\").forward(request, response)");
 	}
 
 }
