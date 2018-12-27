21e29c1c1e24deede22e2626a46de61fc6796693
mrcuijt <mrcuijt@gmail.com>
2018-12-27 16:50:50 +0800

    1、更新 LoginFilter.java 1）添加 Logback 日志 MDC 参数 2）添加日志记录

--- lease-of-house/src/main/java/science/mrcuijt/loh/filter/LoginFilter.java
+++ lease-of-house/src/main/java/science/mrcuijt/loh/filter/LoginFilter.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 /**
  * @author Administrator
@@ -45,12 +46,17 @@ public class LoginFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc)
 			throws IOException, ServletException {
 
+		boolean debug = LOG.isDebugEnabled();
+
 		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
 		// 获取用户请求资源路径
 		String url = httpServletRequest.getRequestURI();
 
+		MDC.put("remoteHost", request.getRemoteHost());
+		MDC.put("localAddr", request.getLocalAddr());
+		MDC.put("sessionId", ((HttpServletRequest) request).getRequestedSessionId());
 		// 判断用户是否是要去登录、登出还是注册
 		if (url.endsWith(httpServletRequest.getContextPath() + "/login.do")
 				|| url.endsWith(httpServletRequest.getContextPath() + "/logout.do")
@@ -58,21 +64,30 @@ public class LoginFilter implements Filter {
 				|| url.endsWith(httpServletRequest.getContextPath() + "/verifyCode.do")
 				|| url.endsWith(httpServletRequest.getContextPath() + "/main/main.do")
 				|| url.endsWith(httpServletRequest.getContextPath() + "/main/getRegion.do")) {
+			LOG.info("requestURI={}", url);
+			LOG.info("serverPath={}", httpServletRequest.getServletPath());
 			fc.doFilter(request, response);
+			MDC.clear();
 			return;
 		}
 
 		// 判断用户是否登录
 		// 获取 Session 中的用户登录标识
 		Object loginInfoId = httpServletRequest.getSession().getAttribute("login_info_id");
-		//
+		// 获取 Session 中的用户表id
+		Object userInfoId = httpServletRequest.getSession().getAttribute("user_info_id");
 		if (loginInfoId == null) {
 			// 跳转到 登录
 			httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/login.do");
+			LOG.info("httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + \"/login.do\");");
+			MDC.clear();
 			return;
 		}
-
+		MDC.put("userid", userInfoId.toString());
+		LOG.info("requestURI={}", url);
+		LOG.info("serverPath={}", httpServletRequest.getServletPath());
 		fc.doFilter(request, response);
+		MDC.clear();
 	}
 
 	/*
@@ -82,7 +97,7 @@ public class LoginFilter implements Filter {
 	 */
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		LOG.debug("init LoginFilter ");
+		LOG.info("init LoginFilter");
 	}
 
 }
