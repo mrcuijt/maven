19e104e5edc2b46c346ae229748238bf9f663deb
mrcuijt <mrcuijt@gmail.com>
2018-12-27 16:41:52 +0800

    1、更新 DeleteLohHouseInfoServlet.java 1）更新参数判断逻辑 2）添加日志记录

--- lease-of-house/src/main/java/science/mrcuijt/loh/servlet/loh/lease/DeleteLohHouseInfoServlet.java
+++ lease-of-house/src/main/java/science/mrcuijt/loh/servlet/loh/lease/DeleteLohHouseInfoServlet.java
@@ -4,12 +4,16 @@
 package science.mrcuijt.loh.servlet.loh.lease;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import science.mrcuijt.loh.entity.LohHouseInfo;
 import science.mrcuijt.loh.service.LohService;
@@ -20,6 +24,8 @@ import science.mrcuijt.loh.service.impl.LohServiceImpl;
  *
  */
 public class DeleteLohHouseInfoServlet extends HttpServlet {
+
+	private static final Logger LOG = LoggerFactory.getLogger(GetRegionServlet.class);
 
 	private LohService lohService = new LohServiceImpl();
 
@@ -33,7 +39,7 @@ public class DeleteLohHouseInfoServlet extends HttpServlet {
 		Integer userInfoId = (Integer) request.getSession().getAttribute("user_info_id");
 
 		// 验证结果标识
-		boolean verifyReuslt = true;
+		boolean verifyResult = true;
 
 		// 获取租赁房屋id
 		Integer lohHouseInfoId = null;
@@ -43,47 +49,68 @@ public class DeleteLohHouseInfoServlet extends HttpServlet {
 
 		// 参数校验
 		try {
-			lohHouseInfoId = Integer.parseInt(request.getParameter("id"));
+			String paramId = request.getParameter("id");
+			if (paramId != null && paramId.trim().length() > 0) {
+				LOG.info("if(paramId != null && paramId.trim().length() > 0) [{}]", (paramId != null && paramId.trim().length() > 0));
+				paramId = paramId.trim();
+				lohHouseInfoId = Integer.parseInt(paramId);
+			}
 		} catch (NumberFormatException e) {
-			verifyReuslt = false;
-			message = "参数错误";
-			e.printStackTrace();
+			LOG.info("Convert lohHouseId [{}] has error", request.getParameter("id"));
+			verifyResult = false;
+			message = "Parameter Not Support";
 		}
 
+		// 验证请求参数
+		if (verifyResult) {
+			if (lohHouseInfoId == null || lohHouseInfoId <= 0) {
+				LOG.info("if (lohHouseId == null) [{}]", (lohHouseInfoId == null));
+				message = "Parameter Not Support";
+				verifyResult = false;
+			}
+		}
+
+		// 验证房屋信息是否存在
 		LohHouseInfo lohHouseInfo = null;
-		if (verifyReuslt) {
+		if (verifyResult) {
+			LOG.info("查询指定房屋租赁信息[{}]", lohHouseInfoId);
 			lohHouseInfo = lohService.findLohHouseInfoByPrimaryKey(lohHouseInfoId);
 			if (lohHouseInfo == null) {
-				verifyReuslt = false;
+				LOG.info("if (lohHouseInfo == null) [{}]", (lohHouseInfo == null));
+				verifyResult = false;
 				message = "房屋租赁信息记录不存在，请重试。";
 			}
 		}
 
 		// 验证是否是该用户发布的信息记录
-		if (verifyReuslt) {
-			if (userInfoId.intValue() != lohHouseInfo.getUserInfoId()) {
-				verifyReuslt = false;
+		if (verifyResult) {
+			if (userInfoId.intValue() != lohHouseInfo.getUserInfoId().intValue()) {
+				LOG.info("if (userInfoId.intValue() != lohHouseInfo.getUserInfoId().intValue()) [{}]", (userInfoId.intValue() != lohHouseInfo.getUserInfoId().intValue()));
+				verifyResult = false;
 				message = "当前用户没有权限修改该房屋信息。";
 			}
 		}
 
 		// 验证不通过
-		if (!verifyReuslt) {
-			// 设置通知消息
-			request.setAttribute("messgage", message);
-			request.getRequestDispatcher("/loh/lease/main.do").forward(request, response);
+		if (!verifyResult) {
+			LOG.info("if (!verifyResult) [{}]", (!verifyResult));
+			message = URLEncoder.encode(message, "UTF-8");
+			response.sendRedirect(request.getContextPath() + "/loh/lease/main.do?message=" + message);
+			LOG.info("response.sendRedirect({}\"/loh/lease/main.do?message={}\")", request.getContextPath(), URLDecoder.decode(message,"UTF-8"));
 			return;
 		}
 
 		// 删除结果
+		LOG.info("删除指定房屋[{}]信息记录", lohHouseInfo.getLohHouseInfoId());
 		boolean deleteLohHouseResult = lohService.deleteLohHouseInfo(lohHouseInfo,
 				request.getServletContext().getRealPath("/"));
 
 		if (!deleteLohHouseResult) {
+			LOG.info("if (!deleteLohHouseResult) [{}]", (!deleteLohHouseResult));
 			message = "删除失败，请重试。";
 			request.setAttribute("message", message);
-			request.getRequestDispatcher("/loh/lease/toDeleteReleaseHouse.do?id=" + lohHouseInfoId).forward(request,
-					response);
+			request.getRequestDispatcher("/loh/lease/toDeleteReleaseHouse.do?id=" + lohHouseInfoId).forward(request, response);
+			LOG.info("request.getRequestDispatcher(\"/loh/lease/toDeleteReleaseHouse.do?id=\" + lohHouseInfoId).forward(request, response)");
 			return;
 		}
 
@@ -92,5 +119,6 @@ public class DeleteLohHouseInfoServlet extends HttpServlet {
 		// 中文参数做 URLEncode 加密
 		message = URLEncoder.encode(message, "UTF-8");
 		response.sendRedirect(request.getContextPath() + "/loh/lease/deleteLohHouseInfoResult.do?message=" + message);
+		LOG.info("response.sendRedirect(request.getContextPath() + \"/loh/lease/deleteLohHouseInfoResult.do?message={})", URLDecoder.decode(message, "UTF-8"));
 	}
 }
