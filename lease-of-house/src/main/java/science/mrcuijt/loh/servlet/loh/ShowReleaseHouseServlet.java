31356a1b693e29c389881a7ff1aa673d85597e8e
mrcuijt <mrcuijt@gmail.com>
2018-12-27 16:37:43 +0800

    1、更新 ShowReleaseHouseServlet.java 1）更新参数判断逻辑 2）添加日志记录

--- lease-of-house/src/main/java/science/mrcuijt/loh/servlet/loh/ShowReleaseHouseServlet.java
+++ lease-of-house/src/main/java/science/mrcuijt/loh/servlet/loh/ShowReleaseHouseServlet.java
@@ -30,7 +30,7 @@ import science.mrcuijt.loh.service.impl.LohServiceImpl;
  */
 public class ShowReleaseHouseServlet extends HttpServlet {
 
-	private static Logger log = LoggerFactory.getLogger(ShowReleaseHouseServlet.class);
+	private static Logger LOG = LoggerFactory.getLogger(ShowReleaseHouseServlet.class);
 
 	private static LohService lohService = new LohServiceImpl();
 
@@ -38,94 +38,129 @@ public class ShowReleaseHouseServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		boolean debug = log.isDebugEnabled();
-
-		String message = null;
-
-		// 获取消息
-		message = request.getParameter("message");
-
-		if (message != null && message.trim().length() > 0) {
-			message = new String(message.getBytes("iso-8859-1"), "UTF-8");
-			request.setAttribute("message", message);
-		}
+		boolean debug = LOG.isDebugEnabled();
 
 		Integer lohHouseInfoId = null;
 
+		String message = null;
+
+		boolean verifyResult = true;
+
 		try {
-			lohHouseInfoId = Integer.parseInt(request.getParameter("id"));
-		} catch (NumberFormatException e) {
-			e.printStackTrace();
-		}
-
-		if (lohHouseInfoId != null && lohHouseInfoId > 0) {
-
-			// 查询房屋信息
-			LohHouseInfo lohHouseInfo = lohService.findLohHouseInfoByPrimaryKey(lohHouseInfoId);
-
-			// 保存房屋信息浏览记录
-			LohHouseViewHistory lohHouseViewHistory = null;
-
-			// 获取登录用户标识
-			Integer loginInfoId = (Integer) request.getSession().getAttribute("login_info_id");
-
-			Integer userInfoId = (Integer) request.getSession().getAttribute("user_info_id");
-
-			// 根据用户登录标识查询用户信息
-			UserInfo userInfo = lohService.findUserInfoByPrimaryKey(userInfoId);
-
-			// 查询房屋信息浏览记录是否存在
-			lohHouseViewHistory = lohService.findLohHouseViewHistoryByLohHouseInfoIdAndUserInfoId(
-					lohHouseInfo.getLohHouseInfoId(), userInfo.getUserInfoId());
-
-			if (lohHouseViewHistory == null) {
-				// 添加新的房屋信息浏览记录
-				lohHouseViewHistory = new LohHouseViewHistory();
-				lohHouseViewHistory.setGmtCreate(new Date());
-				lohHouseViewHistory.setGmtModified(new Date());
-				lohHouseViewHistory.setLohHouseId(lohHouseInfo.getLohHouseInfoId());
-				lohHouseViewHistory.setUserInfoId(userInfo.getUserInfoId());
-				boolean addResult = lohService.addLohHouseViewHistory(lohHouseViewHistory);
-				if (!addResult) {
-					log.info("Add LohHouseViewHistory fail {}", lohHouseViewHistory);
-				}
-			} else {
-				// 更新房屋信息浏览记录
-				lohHouseViewHistory.setGmtModified(new Date());
-				boolean updateResult = lohService.updateLohHouseViewHistoryByPrimaryKey(lohHouseViewHistory);
-				if (!updateResult) {
-					log.info("Upadte LohHouseViewHistory fail {}", lohHouseViewHistory);
-				}
+			String strLohHouseInfoId = request.getParameter("id");
+			if (strLohHouseInfoId != null && strLohHouseInfoId.trim().length() > 0) {
+				LOG.info("if (strLohHouseInfoId != null && strLohHouseInfoId.trim().length() > 0) [{}]", (strLohHouseInfoId != null && strLohHouseInfoId.trim().length() > 0));
+				strLohHouseInfoId = strLohHouseInfoId.trim();
+				lohHouseInfoId = Integer.parseInt(strLohHouseInfoId);
 			}
-
-			// 查询房屋类型信息
-			LohHouseType lohHouseType = lohService.findLohHouseTypeByPrimaryKey(lohHouseInfo.getLohHouseTypeId());
-
-			// 根据房屋信息 Id 查询，房屋文件信息
-			List<LohFileInfo> lohFileInfoList = lohService.findLohFileInfoByLohHouseInfoId(lohHouseInfoId);
-
-			// 根据地区 id 查询地区信息
-			RegionInfo provience = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoProvinceId());
-			RegionInfo city = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCityId());
-			RegionInfo country = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCountyId());
-
-			// 添加房屋信息
-			request.setAttribute("lohHouseInfo", lohHouseInfo);
-
-			// 添加房屋类型
-			request.setAttribute("lohHouseType", lohHouseType);
-
-			// 添加房屋文件信息
-			request.setAttribute("lohFileInfoList", lohFileInfoList);
-
-			// 添加地区信息
-			request.setAttribute("provience", provience);
-			request.setAttribute("city", city);
-			request.setAttribute("country", country);
+		} catch (NumberFormatException e) {
+			LOG.error("Parse lohHouseInfoId has error {}", e.getMessage(), e);
+			message = "Parameter Not Support";
+			verifyResult = false;
 		}
+
+		if(verifyResult) {
+			if (lohHouseInfoId == null || lohHouseInfoId <= 0) {
+				LOG.info("if (lohHouseInfoId == null || lohHouseInfoId <= 0) [{}]", (lohHouseInfoId == null || lohHouseInfoId <= 0));
+				message = "Parameter Not Support";
+				verifyResult = false;
+			}
+		}
+
+		// 查询房屋信息
+		LohHouseInfo lohHouseInfo = null;
+		if (verifyResult) {
+			LOG.info("查询房屋信息[{}]", lohHouseInfoId);
+			lohHouseInfo = lohService.findLohHouseInfoByPrimaryKey(lohHouseInfoId);
+			if (lohHouseInfo == null) {
+				LOG.info("if(lohHouseInfo == null) [{}]", (lohHouseInfo == null));
+				message = "房屋租赁信息记录不存在";
+			}
+		}
+
+		// 验证不通过
+		if (!verifyResult) {
+			request.setAttribute("message", message);
+			request.getRequestDispatcher("/WEB-INF/html/common/error.jsp").forward(request, response);
+			LOG.info("request.getRequestDispatcher(\"/WEB-INF/html/common/error.jsp\").forward(request, response)");
+			return;
+		}
+
+		// 获取登录用户标识
+		Integer loginInfoId = (Integer) request.getSession().getAttribute("login_info_id");
+
+		Integer userInfoId = (Integer) request.getSession().getAttribute("user_info_id");
+
+		// 根据用户登录标识查询用户信息
+		LOG.info("查询用户[{}]信息", userInfoId);
+		UserInfo userInfo = lohService.findUserInfoByPrimaryKey(userInfoId);
+
+		// 保存房屋信息浏览记录
+		LohHouseViewHistory lohHouseViewHistory = null;
+		LOG.info("根据当前用户[{}]查询房屋信息浏览记录[{}]", userInfo.getUserInfoId(), lohHouseInfoId);
+		// 查询房屋信息浏览记录是否存在
+		lohHouseViewHistory = lohService.findLohHouseViewHistoryByLohHouseInfoIdAndUserInfoId(
+				lohHouseInfo.getLohHouseInfoId(), userInfo.getUserInfoId());
+
+		if (lohHouseViewHistory == null) {
+			LOG.info("if (lohHouseViewHistory == null) [{}]", (lohHouseViewHistory == null));
+			// 添加新的房屋信息浏览记录
+			lohHouseViewHistory = new LohHouseViewHistory();
+			lohHouseViewHistory.setGmtCreate(new Date());
+			lohHouseViewHistory.setGmtModified(new Date());
+			lohHouseViewHistory.setLohHouseId(lohHouseInfo.getLohHouseInfoId());
+			lohHouseViewHistory.setUserInfoId(userInfo.getUserInfoId());
+			boolean addResult = lohService.addLohHouseViewHistory(lohHouseViewHistory);
+			if (addResult) {
+				LOG.info("if (addResult) [{}]", (addResult));
+				LOG.info("Add LohHouseViewHistory Success {}", lohHouseViewHistory);
+			} else {
+				LOG.info("if (addResult) [{}]", (addResult));
+				LOG.info("Add LohHouseViewHistory fail {}", lohHouseViewHistory);
+			}
+		} else {
+			LOG.info("if (lohHouseViewHistory == null) [{}]", (lohHouseViewHistory == null));
+			// 更新房屋信息浏览记录
+			lohHouseViewHistory.setGmtModified(new Date());
+			boolean updateResult = lohService.updateLohHouseViewHistoryByPrimaryKey(lohHouseViewHistory);
+			if (updateResult) {
+				LOG.info("if (updateResult) [{}]", (updateResult));
+				LOG.info("Upadte LohHouseViewHistory [{}] Success", lohHouseViewHistory.getLohHouseViewHistoryId());
+				} else {
+				LOG.info("if (updateResult) [{}]", (updateResult));
+				LOG.info("Upadte LohHouseViewHistory [{}] fail", lohHouseViewHistory);
+			}
+		}
+
+		LOG.info("查询房屋类型为[{}]的信息", lohHouseInfo.getLohHouseTypeId());
+		// 查询房屋类型信息
+		LohHouseType lohHouseType = lohService.findLohHouseTypeByPrimaryKey(lohHouseInfo.getLohHouseTypeId());
+
+		LOG.info("查询租赁房屋信息[{}]关联的房屋文件信息", lohHouseInfoId);
+		// 根据房屋信息 Id 查询，房屋文件信息
+		List<LohFileInfo> lohFileInfoList = lohService.findLohFileInfoByLohHouseInfoId(lohHouseInfoId);
+
+		LOG.info("查询指定地区信息[{}]", lohHouseInfo.getRegionInfoProvinceId());
+		// 根据地区 id 查询地区信息
+		RegionInfo provience = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoProvinceId());
+		LOG.info("查询指定地区信息[{}]", lohHouseInfo.getRegionInfoCityId());
+		RegionInfo city = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCityId());
+		LOG.info("查询指定地区信息[{}]", lohHouseInfo.getRegionInfoCountyId());
+		RegionInfo country = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCountyId());
+
+		// 添加房屋信息
+		request.setAttribute("lohHouseInfo", lohHouseInfo);
+		// 添加房屋类型
+		request.setAttribute("lohHouseType", lohHouseType);
+		// 添加房屋文件信息
+		request.setAttribute("lohFileInfoList", lohFileInfoList);
+		// 添加地区信息
+		request.setAttribute("provience", provience);
+		request.setAttribute("city", city);
+		request.setAttribute("country", country);
 
 		request.getRequestDispatcher("/WEB-INF/html/loh/main/show_release_house.jsp").forward(request, response);
-
+		LOG.info("request.getRequestDispatcher(\"/WEB-INF/html/loh/main/show_release_house.jsp\").forward(request, response);");
 	}
 
 }
--- lease-of-house/src/main/java/science/mrcuijt/loh/servlet/loh/lease/ShowReleaseHouseServlet.java
+++ lease-of-house/src/main/java/science/mrcuijt/loh/servlet/loh/lease/ShowReleaseHouseServlet.java
@@ -4,12 +4,17 @@
 package science.mrcuijt.loh.servlet.loh.lease;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import science.mrcuijt.loh.entity.LohFileInfo;
 import science.mrcuijt.loh.entity.LohHouseInfo;
@@ -24,11 +29,17 @@ import science.mrcuijt.loh.service.impl.LohServiceImpl;
  */
 public class ShowReleaseHouseServlet extends HttpServlet {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ShowReleaseHouseServlet.class);
+
 	LohService lohService = new LohServiceImpl();
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		boolean debug = LOG.isDebugEnabled();
+
+		boolean verifyResult = true;
 
 		String message = null;
 
@@ -36,6 +47,7 @@ public class ShowReleaseHouseServlet extends HttpServlet {
 		message = request.getParameter("message");
 
 		if (message != null && message.trim().length() > 0) {
+			LOG.info("if (message != null && message.trim().length() > 0) [{}]", (message != null && message.trim().length() > 0));
 			message = new String(message.getBytes("iso-8859-1"),"UTF-8");
 			request.setAttribute("message", message);
 		}
@@ -43,44 +55,73 @@ public class ShowReleaseHouseServlet extends HttpServlet {
 		Integer lohHouseInfoId = null;
 
 		try {
-			lohHouseInfoId = Integer.parseInt(request.getParameter("id"));
+			String paramId = request.getParameter("id");
+			if (paramId != null && paramId.trim().length() > 0) {
+				paramId = paramId.trim();
+				lohHouseInfoId = Integer.parseInt(paramId);
+			}
 		} catch (NumberFormatException e) {
-			e.printStackTrace();
+			LOG.warn("Convert lohHouseInfoId [{}] has error message=[{}]", request.getParameter("id"), e.getMessage());
+			verifyResult = false;
+			message = "Parameter Not Support";
 		}
 
-		if (lohHouseInfoId != null && lohHouseInfoId > 0) {
-
-			// 查询房屋信息
-			LohHouseInfo lohHouseInfo = lohService.findLohHouseInfoByPrimaryKey(lohHouseInfoId);
-			
-			// 查询房屋类型信息
-			LohHouseType lohHouseType = lohService.findLohHouseTypeByPrimaryKey(lohHouseInfo.getLohHouseTypeId());
-			
-			// 根据房屋信息 Id 查询，房屋文件信息
-			List<LohFileInfo> lohFileInfoList = lohService.findLohFileInfoByLohHouseInfoId(lohHouseInfoId);
-			
-			// 根据地区 id 查询地区信息
-			RegionInfo provience = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoProvinceId());
-			RegionInfo city = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCityId());
-			RegionInfo country = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCountyId());
-			
-			// 添加房屋信息
-			request.setAttribute("lohHouseInfo", lohHouseInfo);
-			
-			// 添加房屋类型
-			request.setAttribute("lohHouseType", lohHouseType);
-			
-			// 添加房屋文件信息
-			request.setAttribute("lohFileInfoList", lohFileInfoList);
-			
-			// 添加地区信息
-			request.setAttribute("provience", provience);
-			request.setAttribute("city", city);
-			request.setAttribute("country", country);
+		if (verifyResult) {
+			if (lohHouseInfoId == null || lohHouseInfoId <= 0) {
+				LOG.info("if (lohHouseInfoId == null || lohHouseInfoId <= 0) [{}]", (lohHouseInfoId == null || lohHouseInfoId <= 0));
+				verifyResult = false;
+				message = "Parameter Not Support";
+			}
 		}
 
+		// 查询房屋信息
+		LohHouseInfo lohHouseInfo = null;
+		if (verifyResult) {
+			LOG.info("查找指定房屋信息[{}]信息", lohHouseInfoId);
+			lohHouseInfo = lohService.findLohHouseInfoByPrimaryKey(lohHouseInfoId);
+			if (lohHouseInfo == null) {
+				LOG.info("if (lohHouseInfo == null) [{}]", (lohHouseInfo == null));
+				verifyResult = false;
+				message = "房屋租赁信息不存在";
+			}
+		}
+
+		if (!verifyResult) {
+			message = URLEncoder.encode(message, "UTF-8");
+			response.sendRedirect(request.getContextPath() + "/loh/lease/main.do?message=" + message);
+			LOG.info("response.sendRedirect(request.getContextPath() + \"/loh/lease/main.do?message=\")", URLDecoder .decode(message, "UTF-8"));
+			return;
+		}
+
+		// 查询房屋类型信息
+		LOG.info("查询指定房屋类型[{}]信息", lohHouseInfo.getLohHouseTypeId());
+		LohHouseType lohHouseType = lohService.findLohHouseTypeByPrimaryKey(lohHouseInfo.getLohHouseTypeId());
+
+		// 根据房屋信息 Id 查询，房屋文件信息
+		LOG.info("查询房屋信息[{}]关联的房屋文件信息", lohHouseInfoId);
+		List<LohFileInfo> lohFileInfoList = lohService.findLohFileInfoByLohHouseInfoId(lohHouseInfoId);
+
+		// 根据地区 id 查询地区信息
+		LOG.info("查询指定地区信息[{}]", lohHouseInfo.getRegionInfoProvinceId());
+		RegionInfo provience = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoProvinceId());
+		LOG.info("查询指定地区信息[{}]", lohHouseInfo.getRegionInfoCityId());
+		RegionInfo city = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCityId());
+		LOG.info("查询指定地区信息[{}]", lohHouseInfo.getRegionInfoCountyId());
+		RegionInfo country = lohService.findRegionInfoByPrimaryKey(lohHouseInfo.getRegionInfoCountyId());
+
+		// 设置房屋信息
+		request.setAttribute("lohHouseInfo", lohHouseInfo);
+		// 设置房屋类型
+		request.setAttribute("lohHouseType", lohHouseType);
+		// 设置房屋文件信息
+		request.setAttribute("lohFileInfoList", lohFileInfoList);
+		// 设置地区信息
+		request.setAttribute("provience", provience);
+		request.setAttribute("city", city);
+		request.setAttribute("country", country);
 
 		request.getRequestDispatcher("/WEB-INF/html/loh/lease/show_release_house.jsp").forward(request, response);
+		LOG.info("request.getRequestDispatcher(\"/WEB-INF/html/loh/lease/show_release_house.jsp\").forward(request, response)");
 	}
 
 }
